c3c9858f76b5ddacfabb3bac7a37898675550a96
Lucas César <lucascesar_35@hotmail.com>
2021-07-09 20:38:52 -0300

    application-test h2 configuration & test profile

--- src/main/java/com/example/desafiozup/model/User.java
+++ src/main/java/com/example/desafiozup/model/User.java
@@ -2,7 +2,6 @@ package com.example.desafiozup.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
