11c59809dcbc20431af6601b8ddfae9280b92043
Lucas César <lucascesar_35@hotmail.com>
2021-07-12 00:15:12 -0300

    Revert "Desfazendo a bagunça com git"
    This reverts commit 7e7f561a17a8fa6b75a0ae72b804a62fdc7c6399.

---
+++ src/main/java/com/example/desafiozup/services/ComicDataWrapper.java
@@ -0,0 +1,27 @@
+package com.example.desafiozup.services;
+
+public class ComicDataWrapper {
+		
+	private ComicDataContainer data;
+	
+	public ComicDataWrapper() {}
+	
+  public ComicDataWrapper(ComicDataContainer data) {
+		super();
+		this.data = data;
+	}
+
+	public ComicDataContainer getData() {
+		return data;
+	}
+
+	public void setData(ComicDataContainer data) {
+		this.data = data;
+	}
+
+	
+
+	
+	
+
+}
---
+++ src/main/java/com/example/desafiozup/services/CreatorList.java
@@ -0,0 +1,28 @@
+package com.example.desafiozup.services;
+
+import java.util.ArrayList;
+
+public class CreatorList {
+	private ArrayList<CreatorSumary> items = new ArrayList<>();
+
+	public CreatorList() {
+		
+	}
+	
+	public CreatorList(ArrayList<CreatorSumary> items) {
+		super();
+		this.items = items;
+	}
+
+	public ArrayList<CreatorSumary> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<CreatorSumary> items) {
+		this.items = items;
+	}
+
+	
+	
+	
+}
